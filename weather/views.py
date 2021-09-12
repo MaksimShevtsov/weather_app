f1234ac03c03e0f2d29c5c772eeaf28454ba30ef
Maksim <xmaksutx@gmail.com>
2021-09-12 22:45:31 +0300

    Com

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
 import requests
+from django.shortcuts import render
+
 # Create your views here.
 
 def index(request):
     city = 'London'
     appid = '01112e9f2a2bae82b21d8155e85ba84a'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city},&appid={appid}'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={},&units=metric&appid=' + appid
+    res = requests.get(url.format(city)).json()
 
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"]["icon"]
+    }
 
     return render(request, 'weather/index.html')
